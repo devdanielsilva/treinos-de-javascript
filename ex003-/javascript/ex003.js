eb5dcb121c1069c861651b9ad20f98f76017aeb0
Daniel Silva <devdanielsilva@gmail.com>
2023-07-07 22:23:19 -0300

    .

--- ex003-/javascript/ex003.js
+++ ex003-/javascript/ex003.js
@@ -1,1 +1,7 @@
 alert('Mais um exercício')
+
+document.querySelectorAll('.btn').forEach((item) => {
+    item.addEventListener('click', () => {
+        alert('Olá seja     b   em Vindo ao ex003')
+    })
+})
