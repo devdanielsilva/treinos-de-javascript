c677bcc5543f4a367fe3f618e775b13619d38bfe
Daniel Silva <devdanielsilva@gmail.com>
2023-07-07 22:59:42 -0300

    Update ex003.js

--- ex003-/javascript/ex003.js
+++ ex003-/javascript/ex003.js
@@ -6,6 +6,6 @@ document.querySelectorAll('.btn').forEach((item) => {
     })
 })
 
-var n1 = parseInt(prompt("Digite um número"));
-var n2 = parseInt(prompt("Digite um número"));
+var n1 = parseInt(prompt("Digite o primeiro número"));
+var n2 = parseInt(prompt("Digite o segundo número"));
 alert( "O resultado da soma é: "+ (n1 + n2));
