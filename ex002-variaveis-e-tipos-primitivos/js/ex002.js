9d74ead6791a100e347db011a30c6445d902027c
Daniel Silva <devdanielsilva@gmail.com>
2023-07-08 23:10:06 -0300

    .

--- ex002-variaveis-e-tipos-primitivos/js/ex002.js
+++ ex002-variaveis-e-tipos-primitivos/js/ex002.js
@@ -3,3 +3,15 @@ alert('Treino de Javascript - ex002'); //Tela de alerta com uma mensagem.
 
 confirm('Ir para o ex002');//Tela de confirmação. Ok e Cancelar
 
+
+/*O script abaixo é de um botão. */
+document.querySelectorAll('.btn').forEach((item) => {
+    item.addEventListener('click', () => {
+        alert('Olá seja bem Vindo ao ex003')
+    })
+});
+
+/*O script abaixo é de soma simples de duas variáveis. */
+var n1 = parseInt(prompt("Digite o primeiro número"));
+var n2 = parseInt(prompt("Digite o segundo número"));
+alert("O resultado da soma é: " + (n1 + n2));
--- ex003-/javascript/ex003.js
+++ ex003-/javascript/ex003.js
@@ -1,15 +1,4 @@
 alert('Mais um exercício');
 window('Vamos para mais um exercício ?');
 
-/*O script abaixo é de um botão. */
-document.querySelectorAll('.btn').forEach((item) => {
-    item.addEventListener('click', () => {
-        alert('Olá seja bem Vindo ao ex003')
-    })
-});
 
-
-/*O script abaixo é de soma simples de duas variáveis. */
-var n1 = parseInt(prompt("Digite o primeiro número"));
-var n2 = parseInt(prompt("Digite o segundo número"));
-alert("O resultado da soma é: " + (n1 + n2));
