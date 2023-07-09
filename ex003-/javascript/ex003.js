c9d9f6d827cba97bab0fa08e2f0ae4dbd105f3a3
Daniel Silva <devdanielsilva@gmail.com>
2023-07-08 23:20:26 -0300

    Update ex003.js

--- ex003-/javascript/ex003.js
+++ ex003-/javascript/ex003.js
@@ -1,4 +1,4 @@
-window('Vamos para mais um exercício ?');
-
-var nome = prompt('Qual é o seu nome?');
-alert('É um grande prazer em te conhecer, ' + nome);
+/*Abaixo é uma variável com um prompt para receber o nome do usuário.  */
+var nome = window.prompt('Qual é o seu nome?');
+/*Abaixo o alert que vai mostrar uma mensagem junto do nome do usuário. Nome que está armazenado na variável nome. */
+alert('É um grande prazer em te conhecer, ' + nome); //O sinal de + é sinal de concatenação.
