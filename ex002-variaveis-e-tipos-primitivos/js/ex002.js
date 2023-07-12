389e8e09bc1d521633d059b6c8f91c1641ce046b
Daniel Silva <devdanielsilva@gmail.com>
2023-07-12 16:49:06 -0300

    Update ex002.js

--- ex002-variaveis-e-tipos-primitivos/js/ex002.js
+++ ex002-variaveis-e-tipos-primitivos/js/ex002.js
@@ -12,6 +12,8 @@ document.querySelectorAll('.btn').forEach((item) => {
 });
 
 /*O script abaixo é de soma simples de duas variáveis. */
-var n1 = parseInt(prompt("Digite o primeiro número"));
-var n2 = parseInt(prompt("Digite o segundo número"));
+var n1 = parseFloat(prompt("Digite o primeiro número"));
+var n2 = parseFloat(prompt("Digite o segundo número"));
 alert("O resultado da soma é: " + (n1 + n2));
+
+
