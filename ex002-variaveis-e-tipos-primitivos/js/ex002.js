47706a73d90593723cecd6e9e5613f6ae42f5415
Daniel Silva <devdanielsilva@gmail.com>
2023-07-12 17:08:43 -0300

    .

--- ex002-variaveis-e-tipos-primitivos/js/ex002.js
+++ ex002-variaveis-e-tipos-primitivos/js/ex002.js
@@ -7,13 +7,15 @@ confirm('Ir para o ex002');//Tela de confirmação. Ok e Cancelar
 /*O script abaixo é de um botão. */
 document.querySelectorAll('.btn').forEach((item) => {
     item.addEventListener('click', () => {
-        alert('Olá seja bem Vindo ao ex002')
+        alert('Olá seja bem Vindo ao ex002' + '!')
     })
 });
 
-/*O script abaixo é de soma simples de duas variáveis. */
+/*O script abaixo é de soma simples de duas variáveis.
+
+É um script de soma de duas notas, seja com vírgula ou não.*/
 var n1 = parseFloat(prompt("Digite o primeiro número"));
 var n2 = parseFloat(prompt("Digite o segundo número"));
-alert("O resultado da soma é: " + (n1 + n2) + !);
+alert("O resultado da soma é: " + (n1 + n2) + '!');
 
 
--- ex003-/javascript/ex003.js
+++ ex003-/javascript/ex003.js
@@ -5,3 +5,10 @@ alert('É um grande prazer em te conhecer, ' + nome + '!'); /*O sinal de + é si
 Quando queremos que um alert mostre uma mensagem e o valor da variável nome, adicionamos fora das aspas da mensagem, seja aspas simples ou duplas, adicionamos o sinal de concatenação + e o nome da variável que tem o valor que queremos que mostre junto desta mensagem.
 
 Para adicionar uma esclamação no final da mensagem com o nome, é só colocar o sinal de concatenação + e coloca dentro de aspas o sinal de esclamação.*/
+
+/*O script abaixo é de um botão. */
+document.querySelectorAll('.btn').forEach((item) => {
+    item.addEventListener('click', () => {
+        alert('Olá seja bem Vindo ao ex003' + '!')
+    })
+});
