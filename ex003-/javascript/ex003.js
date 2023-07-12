cc41d62aba0d0b1ee86b44813fda2d1b64426afc
Daniel Silva <devdanielsilva@gmail.com>
2023-07-12 16:29:14 -0300

    Update ex003.js

--- ex003-/javascript/ex003.js
+++ ex003-/javascript/ex003.js
@@ -1,4 +1,4 @@
 /*Abaixo é uma variável com um prompt para receber o nome do usuário.  */
 var nome = window.prompt('Qual é o seu nome?');
 /*Abaixo o alert que vai mostrar uma mensagem junto do nome do usuário. Nome que está armazenado na variável nome. */
-alert('É um grande prazer em te conhecer, ' + nome); //O sinal de + é sinal de concatenação.
+alert('É um grande prazer em te conhecer, ' + nome); /*O sinal de + é sinal de concatenação. Quando queremos que um alert mostre uma mensagem e o valor da variável nome, adicionamos fora das aspas da mensagem, seja aspas simples ou duplas, adicionamos o sinal de concatenação e o nome da variável que tem o valor que queremos que mostre junto desta mensagem.*/
