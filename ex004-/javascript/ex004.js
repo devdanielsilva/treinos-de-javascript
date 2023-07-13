22f3b56e8140ebaa3c2460d7e62dfde31a995108
Daniel Silva <devdanielsilva@gmail.com>
2023-07-13 19:24:03 -0300

    .

---
+++ ex004-/javascript/ex004.js
@@ -0,0 +1,3 @@
+var nome = prompt('Qual é o seu nome?'); /*Nesse caso aqui, o prompt é uma tela com um botão de ok. Serve para mostrar algo.*/
+
+document.write('Olá, ${nome}! Seu nome tem ${nome.length} letras.'); /**/
