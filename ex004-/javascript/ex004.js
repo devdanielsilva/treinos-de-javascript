ef9d606dacf81b724951264a4b237bc8b8e52f82
Daniel Silva <devdanielsilva@gmail.com>
2023-07-13 19:33:13 -0300

    Update ex004.js

--- ex004-/javascript/ex004.js
+++ ex004-/javascript/ex004.js
@@ -1,6 +1,10 @@
 var nome = prompt('Qual é o seu nome?'); /*Nesse caso aqui, o prompt é uma tela com um botão de ok. Serve para mostrar algo.*/
 
-document.write(`Olá, ${nome}! Seu nome tem ${nome.length} letras.`); /*document.write escreve na tela direto, sem precisar de telinha.*/
+document.write(`Olá, ${nome}! Seu nome tem ${nome.length} letras.`); /*document.write escreve na tela direto, sem precisar de telinha.
+
+Esse nome.length serve para saber o tamanho de caracteres.
+
+length é o comprimento de algo.*/
 
 /*DIFERENÇA DE ASPAS SIMPLES E SINAL DE CRASE:
 SÍMBOLO DE CRASE É ESSE: `
