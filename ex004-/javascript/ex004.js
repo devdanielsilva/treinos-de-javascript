dd7120eaca6fea1fa5f3af3234e323a1cc311cca
Daniel Silva <devdanielsilva@gmail.com>
2023-07-13 19:31:25 -0300

    Update ex004.js

--- ex004-/javascript/ex004.js
+++ ex004-/javascript/ex004.js
@@ -1,3 +1,8 @@
 var nome = prompt('Qual é o seu nome?'); /*Nesse caso aqui, o prompt é uma tela com um botão de ok. Serve para mostrar algo.*/
 
-document.write('Olá, ${nome}! Seu nome tem ${nome.length} letras.'); /*document.write escreve na tela direto, sem precisar de telinha.*/
+document.write(`Olá, ${nome}! Seu nome tem ${nome.length} letras.`); /*document.write escreve na tela direto, sem precisar de telinha.*/
+
+/*DIFERENÇA DE ASPAS SIMPLES E SINAL DE CRASE:
+SÍMBOLO DE CRASE É ESSE: `
+SÍMBOLO DE ASPAS SIMPLES É ESSE: '
+*/
