e1b64b0874c9edab7513240c5595bb331c3b53e0
Daniel Silva <devdanielsilva@gmail.com>
2023-07-12 21:03:02 -0300

    Update ex002.js

--- ex002-variaveis-e-tipos-primitivos/js/ex002.js
+++ ex002-variaveis-e-tipos-primitivos/js/ex002.js
@@ -23,12 +23,13 @@ var soma = n1 + n2;
 alert("O resultado da soma é: " + soma);
 
 /*O sinal de mais + , tem o efeito de adição e também tem o efeito de concatenação.
-
+--------------------------------------------------------
 Para o sinal de mais + ser usado para adição, tem que ser, number + number. Para ser utilizado para uma concatenação, tem que ser, string + string.
-
+--------------------------------------------------------
 Para soma de números inteiros, usamos parseInt.
 Para soma de números Reais, usamos o parseFloat.
-
+--------------------------------------------------------
+O window.prompt ou só prompt, nos retorna valores como string, mesmo que seja números Inteiros ou Reais.
 */
 
 /*----------------------------------------------------*/
