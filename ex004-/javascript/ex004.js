76f76c6b2d948a4085f79288857aa78c327d1950
Daniel Silva <devdanielsilva@gmail.com>
2023-07-13 21:18:31 -0300

    Update ex004.js

--- ex004-/javascript/ex004.js
+++ ex004-/javascript/ex004.js
@@ -9,6 +9,9 @@ length é o comprimento de algo.*/
 /*DIFERENÇA DE ASPAS SIMPLES E SINAL DE CRASE:
 SÍMBOLO DE CRASE É ESSE: `
 SÍMBOLO DE ASPAS SIMPLES É ESSE: '
+--------------------------------------------------
+USAR <strong> NO ${nome}, não tem problema.
+A tag strong deixa a palavra ou trecho de texto em negrito de forma semântica.
 */
 //------------------------------------------------
 document.write(`Seu nome em letras maiúsculas é ${nome.toUpperCase()} `); /*O Atributo toUpperCase, serve para deixar os caracteres em LETRAS MAIÚSCULAS.*/
