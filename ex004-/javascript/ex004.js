ac9fd6e95f1fe1fe2a69ae12c53d99334ef4db13
Daniel Silva <devdanielsilva@gmail.com>
2023-07-14 23:31:33 -0300

    Update ex004.js

--- ex004-/javascript/ex004.js
+++ ex004-/javascript/ex004.js
@@ -18,5 +18,24 @@ Quebra de linha, antes do final das aspas, independente qual seja o tipo de aspa
 //------------------------------------------------
 document.write(`Seu nome em letras maiúsculas é ${nome.toUpperCase()} <br/>`); /*O Atributo toUpperCase, serve para deixar os caracteres em LETRAS MAIÚSCULAS.*/
 /*----------------------------------------------*/
-document.write(`Seu nome em minúsculo é ${nome.toLowerCase} <br/>`) /*O Atributo toLowerCaser, server para deixar os caracteres em letras minúsculas.*/
+document.write(`Seu nome em minúsculo é ${nome.toLowerCase()} <br/>`) /*O Atributo toLowerCaser, server para deixar os caracteres em letras minúsculas.*/
 /*----------------------------------------------*/
+/*FORMATANDO NÚMEROS EM JAVASCRIPT
+
+EXEMPLO: Se temos uma variável que vai receber um valor acima de 1000, e queremos que esse número tenha 2 casas decimais. 
+
+EXLPICAÇÃO LOGO ABAIXO: */
+
+var n1 = 1545.5; /*Para mostrar esse número com 2 casas decimais, fazemos o seguinte. Digitamos o nome da variável que tenha os números que vai receber atributos para serem mostrados com 2 casas decimais.*/
+
+alert('Número com 2 casas decimais é: ' + n1.toFixed(2)); /*O Comando toFixed(2) serve para mostrar os números com as casas decimais. Nesse exemplo foi 2 casas decimais, mas pode ser mais.
+--------------------------------------------------
+Só que tem um problema, no lugar do ponto é uma virgula, aqui no Brasil usamos bastante virgulas.
+
+Para resolver este problema, fazemos o seguinte: Utilizamos toFixed(2).replace('.', ','). 
+
+Basicamente o que vai acontecer é, vamos trocar o ponto pela virgula, para isso ser feito, dentro de aspas colocamos um . ponto, que é o ponto que vai ser trocado, depois uma virgula no meio, abre aspas coloca , uma virgula e fecha aspas. 
+Ai o ponto vai ser trocado. 
+*/
+
+n1.toFixed(2).replace('.', ',');
