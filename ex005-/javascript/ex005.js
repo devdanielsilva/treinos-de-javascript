5c3c1c3879d36e74c4d9fc8a1212a02ffa144a33
Daniel Silva <devdanielsilva@gmail.com>
2023-07-19 22:04:33 -0300

    Update ex005.js

--- ex005-/javascript/ex005.js
+++ ex005-/javascript/ex005.js
@@ -5,3 +5,4 @@ Seja na matemática e na programação, devemos tomar muito cuidado com a PRECED
 A precedência de operadores **determina a ordem em que os operadores são processados**. Operadores com maior precedência são processados primeiro. 
 
 EXEMPLO: O operador de multiplicação (" * ") tem maior precedência que o operador de adição (" + ") e por isso será executado primeiro.*/
+/*-------------------------------------------*/
