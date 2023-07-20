d8c29ccd0fd3f3c67c521e6d95e92a7d75307064
Daniel Silva <devdanielsilva@gmail.com>
2023-07-19 21:04:44 -0300

    Update ex005.js

--- ex005-/javascript/ex005.js
+++ ex005-/javascript/ex005.js
@@ -1,3 +1,7 @@
 /*CUIDADOS QUE DEVEMOS TOMAR COM OS OPERADORES.
 
-Seja na matemática e na programação, */
+Seja na matemática e na programação, devemos tomar muito cuidado com a PRECEDÊNCIA DE OPERADORES.
+
+A precedência de operadores **determina a ordem em que os operadores são processados**. Operadores com maior precedência são processados primeiro. 
+
+EXEMPLO: O operador de multiplicação (" * ") tem maior precedência que o operador de adição (" + ") e por isso será executado primeiro.*/
