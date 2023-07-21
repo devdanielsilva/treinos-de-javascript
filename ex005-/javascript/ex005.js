682cb5f3dc39d08386664c740b0a8c01c4b6f1b2
Daniel Silva <devdanielsilva@gmail.com>
2023-07-20 22:28:37 -0300

    Update ex005.js

--- ex005-/javascript/ex005.js
+++ ex005-/javascript/ex005.js
@@ -5,4 +5,75 @@ Seja na matemática e na programação, devemos tomar muito cuidado com a PRECED
 A precedência de operadores **determina a ordem em que os operadores são processados**. Operadores com maior precedência são processados primeiro. 
 
 EXEMPLO: O operador de multiplicação (" * ") tem maior precedência que o operador de adição (" + ") e por isso será executado primeiro.*/
-/*-------------------------------------------*/
+/*-------------------------------------------
+Operadores do Javasrcipt
+Existem vários operadores no JAVASCRIPT.
+
+Neste curso de Javascript do curso em vídeo vimos:
+
+Operadores aritméticos, Operadores de atribuição, Operadores relacionais, Operadores lógicos e Operadores ternário.
+*/
+/*------------------------------------------
+Operadores aritméticos
+- + Adição
+- - Subtração
+- * Multiplicação
+- / Divisão
+- % Resto da divisão inteira (É confundido com Porcentagem). Para uma divisão de resto da divisão inteira, ao decorrer da divisão, quando chegar o momento que tiver que colocar uma virgula ao lado do número, paramos de dividir.
+
+EXEMPLO DE RESTO DE DIVISÃO INTEIRA: 5/ 2 = 2 Não colocamos a virgula ou ponto e o 5, colocamos 2 sozinho. 5 dividido por 2 é 2, 2*2 2 vezes 2, é 4, para chegar a 5, resta 1. Depois disso paramos por aqui, porque se não, teremos que colocar uma virgula do lado do 2 e o 0 zero do lado do 1 e continua dividindo até chegar o resto da divisão 0 zero.
+
+Mas quando queremos o números do resto da divisão inteira, paramos no momento em que tiver que colocar a virgula. NESTE EXEMPLO: O Resto da divisão inteira, é 1 um.
+
+- **  O sinal de multiplicar 2 vezes junto, não é multiplicação, mas sim o sinal de potencia.
+
+EXEMPLO: 5**2 = 25 (5 ao quadrado * vezes 2, é 25).
+*/
+/*----------------------------------------
+## CUIDADOS AO USAR OPERADORES
+
+Quanto na matemática e na programação, devemos tomar muito cuidado com a PRECEDÊNCIA DE OPERADORES.
+
+A precedência de operadores **determina a ordem em que os operadores são processados**. Operadores com maior precedência são processados primeiro. 
+
+EXEMPLO: 
+
+`3 + 4 * 5 // returns 23`
+
+Primeiro fazemos a multiplicação e depois a adição.
+
+4 * 5 (4 * vezes 5, é 20) (Pegamos o resultado 20 e fazemos a adição com 3.) (3 + 20 = 23)
+
+ O operador de multiplicação (" * ") tem maior precedência que o operador de adição (" + ") e por isso será executado primeiro.
+
+ ..........................................
+ EXEMPLO NÚMERO 2: 
+ 5 + 3 / 2 O que diriam sem saber das regras de uso dos operadores, é 4. Mas isso está errado, pois, nessa operação, começamos dividindo primeiro antes de fazer a adição.
+
+ FICARÁ ASSIM: 3 / 2 = 1,5 
+
+3 dividido por 2, é 1,5.
+
+DEPOIS FAZEMOS A ADIÇÃO, QUE É A SEGUNDA PARTE DA OPERAÇÃO.
+Depois de fazer a divisão, faremos a adição.
+
+5+ 1.5 = 6.5
+
+5 + mais 1.5 é igual = 6.5
+...........................................
+MAS CASO QUEREMOS FAZER PRIMEIRO A ADIÇÃO DE 5 + 3
+
+É só colocarmos parênteses nos número e da expressão de adição para podermos fazer isso. Fazendo isso mudamos a ordem de precedente. Ou seja, a ordem de importância.
+Ai nesse caso, invés de 3 / dividido por 2, que é 4.
+
+Temos 5 + 3 = 8
+
+Eai podemos prosseguir.
+5 + 3 = 8 / 2 = 4
+
+*/
+/
+/*------------------------------------------
+ATRIBUIÇÃO SIMPLES
+
+*/
