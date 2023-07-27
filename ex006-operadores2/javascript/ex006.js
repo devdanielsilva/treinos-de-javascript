20f72ad5566d3e06729dcd6e940875d599a0e4c9
Daniel Silva <devdanielsilva@gmail.com>
2023-07-27 16:19:07 -0300

    Update ex006.js

--- ex006-operadores2/javascript/ex006.js
+++ ex006-operadores2/javascript/ex006.js
@@ -1,3 +1,3 @@
-/*Para ter o alert mostrando junto de um prompt, basta declarar a variável sinal de recebe = prompt('Mensagem para o usuário digitar o nome dele, aqui dentro.'); coloca um alert logo depois do comando de prompt. alert('Mensagem que o usuário irá ver, vai aqui dentro.' + nome da variável que recebeu o nome do usuário + sinal de esclamação '!');
+/*Para ter o alert mostrando junto de um prompt, basta declarar a variável sinal de recebe = prompt('Mensagem para o usuário digitar o nome dele, aqui dentro.'); coloca um alert logo depois do comando de prompt. alert('Mensagem que o usuário irá ver, vai aqui dentro.' + nome da variável que recebeu o nome do usuário + sinal de exclamação '!');
 como mostra logo abaixo.*/
 var saudacao = prompt('Digite seu nome'); alert('Bem-vindo a aula de operadores parte 2 ' + saudacao + '!');
