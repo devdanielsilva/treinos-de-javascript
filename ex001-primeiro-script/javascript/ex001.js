a2e85b0bc74f5cf93f3c5e293ba7186bd313617d
Daniel Silva <devdanielsilva@gmail.com>
2023-07-27 17:47:22 -0300

    Update ex001.js

--- ex001-primeiro-script/javascript/ex001.js
+++ ex001-primeiro-script/javascript/ex001.js
@@ -5,17 +5,17 @@
         Esse comando pode ser usado com o window. na frete. EXEMPLO: window.alert(''); mas não é necessário, só o alert com parentese e aspas ('') está bom.
         
         O alert só te dá a opção de um botão Ok.*/
-        alert('Minha primeira mensagem.');
+alert('Minha primeira mensagem.');
 
-        /*window.confirm(''); serve para te mostrar uma janela com uma mensagem de confirmação, ou seja, uma mensagem para o usuário confirmar.
-        
-        Esse comando pode ser usado com o window. na frente ou não. Seguindo o mesmo exemplo do alert.
-        
-        O confirm te dá dois botões de interação, que são o Ok e o cancela, mas nada te impede de mudar para Sim ou Não.*/
+/*window.confirm(''); serve para te mostrar uma janela com uma mensagem de confirmação, ou seja, uma mensagem para o usuário confirmar.
+ 
+Esse comando pode ser usado com o window. na frente ou não. Seguindo o mesmo exemplo do alert.
+ 
+O confirm te dá dois botões de interação, que são o Ok e o cancela, mas nada te impede de mudar para Sim ou Não.*/
 
-        /*Podemos pular no javascript, tanto no alert, confirm, prompt ou em qualquer outro.
-        Basta usar junto ao texto ou em qualquer outro lugar, o \n*/
-        window.confirm('Mais um comando em \nJavascript.');
+/*Podemos pular linha em uma mensagem no javascript, tanto no alert, confirm, prompt ou em qualquer outro.
+Basta usar junto ao texto ou em qualquer outro lugar, o \n*/
+window.confirm('Mais um comando em \nJavascript.');
 
-        /*window.prompt ou só prompt() exibe uma caixa de diálogo com uma mensagem opcional solicitando ao usuário a entrada de algum texto ou entrada de dados diversos.*/
-        window.prompt('Qual é o seu o nome?');
+/*window.prompt ou só prompt() exibe uma caixa de diálogo com uma mensagem opcional solicitando ao usuário a entrada de algum texto ou entrada de dados diversos.*/
+window.prompt('Qual é o seu o nome?');
