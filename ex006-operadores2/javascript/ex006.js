4934733ec2bf824038d6b9ec28004ff203e6ba02
Daniel Silva <devdanielsilva@gmail.com>
2023-08-04 22:11:21 -0300

    Update ex006.js

--- ex006-operadores2/javascript/ex006.js
+++ ex006-operadores2/javascript/ex006.js
@@ -15,4 +15,12 @@ Operador de diferente é o !=
 
 Para toda expressão que tenha um operador relacional ligado a ela, o resultado dessa expressão vai sempre ser um valor booleano. Um valor do tipo Verdadeiro ou Falso.
 
+EXEMPLOS COM OS OPERADORES RELACIONAIS:
+5 >2 É verdadeiro (true), porque 5 é maior que 2.
+7 < 4 É falso (false), porque 7 não é menor que 4.
+8 >= 8 É verdadeiro (true), porque 8 não maior que 8 mas, é igual a 8.
+9 <= 7 É falso (false), porque 9 não é menor e nem igual a 7.
+5 == 5 É verdadeiro (true), porque 5 é igual a 5. No Javascript o sinal de igual é, dois sinais de igual juntos == Um único sinal de igual = é, sinal de recebe, ou seja, sinal de atribuição.
+4 != 4 Esse sinal de exclamação e sinal de igual, é sinal de não-igual, e uma coisa que não é igual, é diferente. Nesse caso é Falso (false), porque 4 é igual a 4.
+
 */
