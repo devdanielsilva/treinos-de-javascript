16d20e224b0409b661af16a0602cac26a071acbb
Daniel Silva <devdanielsilva@gmail.com>
2023-08-21 15:14:40 -0300

    Treino de botão
    Treino de mensagem ao clicar no botão.

---
+++ ex007-/javascript/ex007.js
@@ -0,0 +1,9 @@
+/*Para chamar uma função, o function com o nome da função e parenteses na frete do nome da função.
+EXEMPLO: 
+funcion minhaFuncao(){
+    alert("Olá, Mundo!");
+}*/
+
+function minhaFuncao(){
+    alert("Olá, Mundo!");
+}
