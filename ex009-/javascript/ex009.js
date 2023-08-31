c8b7c427b6cecb26d69adfaf9b9dbc29319aa855
Daniel Silva <devdanielsilva@gmail.com>
2023-08-31 15:17:21 -0300

    .

---
+++ ex009-/javascript/ex009.js
@@ -0,0 +1,38 @@
+/*Se quisermos mostrar coisas na tela com o window.document.white(window.navigator.appName); 
+    Que serve para mostrar o nome do navegador que você está usando. */
+
+alert("Olá mundo");
+
+    window.document.write(window.navigator.appName) ; //Mostra o nome do navegador
+    "<hr>";
+
+    /* */
+    window.document.write(window.document.URL); //Mostra a URL do site.
+
+    /* SELEÇÃO POR NOME DE PARÁGRAFO
+    A variável acima está selecionando o primeiro parágrafo, por meio de colchetes [0] dentro de colchetes, colocamos o número que corresponde a posição do parágrafo. 
+    var p1 = document.getElementsByName('p')[0];
+
+    ESCREVER UM PARÁGRAFO NA TELA
+    document.write(); serve para escrever na tela, o texto do parágrafo selecionado.
+
+    window.document.write('Está escrito assim: ' + p1.innerText); 
+    */
+
+    /* MUDAR A COR DE UM TEXTO DE UM PARÁGRAFO
+    p1.style.color = 'red';
+    serve para a cor do primeiro parágrafo. 
+    
+    É um exemplo para mudar a cor de fundo do body de um documento HTML.
+     var corpo = window.document.body;
+    corpo.style.background = 'black';
+    */
+
+    /* SELEÇÃO POR ID
+    var d = window.document.getElementById('msg');
+    d.style.background = 'red';
+    sado para alterar a cor da div com id msg.
+    */
+
+
+
