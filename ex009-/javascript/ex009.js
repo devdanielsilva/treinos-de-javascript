3ef67c0820a797dacf5e023a0691f735de91b1d1
Daniel Silva <devdanielsilva@gmail.com>
2023-08-31 14:20:01 -0300

    Update ex009.js

--- ex009-/javascript/ex009.js
+++ ex009-/javascript/ex009.js
@@ -5,3 +5,8 @@ window.document.write(window.navigator.appName) ; //Mostra o nome do navegador
 
 /* */
 window.document.write(window.document.URL); //Mostra a URL do site.
+
+var p1 = document.getElementsByName('p')[0];
+/*A variável acima está selecionando o primeiro parágrafo, por meio de colchetes [0] dentro de colchetes, colocamos o número que corresponde a posição do parágrafo. */
+
+window.document.write('Está escrito assim: ' + p1.innerText); /*document.write(); serve para escrever na tela, o texto do parágrafo selecionado. */
