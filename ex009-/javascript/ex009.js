24a62b0d268d79563b6ea516ab8b1fc83528db81
Daniel Silva <devdanielsilva@gmail.com>
2023-08-31 12:47:45 -0300

    Update ex009.js

---
+++ ex009-/javascript/ex009.js
@@ -0,0 +1,7 @@
+/*Se quisermos mostrar coisas na tela com o window.document.white(window.navigator.appName); 
+Que serve para mostrar o nome do navegador que você está usando. */
+window.document.write(window.navigator.appName) ; //Mostra o nome do navegador
+ "<hr>";
+
+/* */
+window.document.write(window.document.URL); //Mostra a URL do site.
