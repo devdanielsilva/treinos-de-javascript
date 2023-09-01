b70d5b5aa4170240a1571a782e5d07a1b62c06d6
Daniel Silva <devdanielsilva@gmail.com>
2023-09-01 15:57:31 -0300

    .

--- ex010-calculo-do-jantar/javascript/ex010.js
+++ ex010-calculo-do-jantar/javascript/ex010.js
@@ -1,4 +1,4 @@
-const jantar = Number(prompt("Valor do Jantar R$: ")); //Lê o valor do jantar.
+//const jantar = Number(prompt("Valor do Jantar R$: ")); //Lê o valor do jantar.
 const garcom = jantar * 0.10; //Calcula os 10% do garçom.
 const total = jantar + garcom; //Resultado final do jantar.
-alert('Taxa Garçom R$: ${garcom.toFixed(2)}\nTotal R$: ${total.toFixed(2)}');
+//alert('Taxa Garçom R$: ${garcom.toFixed(2)}\nTotal R$: ${total.toFixed(2)}');
