9a335d59e279a687720b7bdd6ba3ca047b9a7fe2
Daniel Silva <devdanielsilva@gmail.com>
2023-09-15 15:59:35 -0300

    Update ex011.js

--- ex011-/javascript/ex011.js
+++ ex011-/javascript/ex011.js
@@ -1,6 +1,6 @@
-var idade = pasrseInt(prompt('Digite sua idade'));
+var idade = parseInt(prompt('Digite sua idade')); //Variável que vai receber a idade do usuário.
 if(idade <= 17){
-    print('Você é menor de idade!');
+    document.write('Você é menor de idade!');
 }else if(idade >= 18){
-    print('Você é maior de idade, e você já pode ser preso"');
+    document.write('Você é maior de idade, e você já pode ser preso');
 }
