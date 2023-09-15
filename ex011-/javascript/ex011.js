3de244c755af8c6bda6cc486297a9b214fc06eb2
Daniel Silva <devdanielsilva@gmail.com>
2023-09-15 15:45:05 -0300

    .

---
+++ ex011-/javascript/ex011.js
@@ -0,0 +1,6 @@
+var idade = parseInt(prompt('Digite sua idade'));
+if(idade <= 17){
+    alert('Você é menor de idade!');
+} else if(idade >= 18){
+    alert('Você é maior de idade, você já pode ser preso!');
+}
