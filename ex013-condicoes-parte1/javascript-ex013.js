ab1243c07056b2899585d2f3435001dad62bfe24
Daniel Silva <devdanielsilva@gmail.com>
2023-10-12 22:08:32 -0300

    .

---
+++ ex013-condicoes-parte1/javascript-ex013.js
@@ -0,0 +1,7 @@
+var velocidade = prompt('Digite a Velocidade do Carro?');
+if (velocidade <= 15){
+    echo("Velocidade Normal!");
+    echo("Dirija Sempre com cautela.");
+}else if(velocidade >= 70){
+    echo("Você está acima da Velocidade e Será Multado.");
+}
