5b9044f18730b1c49a5975faa54298ddfef447d1
Daniel Silva <devdanielsilva@gmail.com>
2023-10-12 21:13:39 -0300

    Update ex011.js

--- ex011-idade-do-usuario-com-if-e-else_if/javascript/ex011.js
+++ ex011-idade-do-usuario-com-if-e-else_if/javascript/ex011.js
@@ -1,6 +1,6 @@
 var idade = parseInt(prompt('Digite sua idade')); //Variável que vai receber a idade do usuário.
 if(idade <= 17){//if = se for menor de idade, exibir texto.
-    document.write('Você é menor de idade!');
+    document.write('Você é Menor de idade!');
 }else if(idade >= 18){//else if = senão for menor de idade exibir mesangem.
-    document.write('Você é maior de idade, e você já pode ser preso');
+    document.write('Você é maior de idade, e Você já pode ser Preso!');
 }
