e1cd8c18ba327d9f007408b6e93020997abdbaff
Daniel Silva <devdanielsilva@gmail.com>
2023-10-13 14:54:50 -0300

    Sistemas de Multa

--- ex013-condicoes-parte1/javascript-ex013.js
+++ ex013-condicoes-parte1/javascript-ex013.js
@@ -1,4 +1,12 @@
-var velocidade = 61;
+/*A variável dentro da função, somente essa função pode acessar a variável, os valores dela. */
+function calcular(){
+    var txtv = document.querySelector('input#txtvel');
+    var res = document.querySelector('div#res')//Para acessar o id no querySelector, se usa o #
+    //Id usa # e Class se usa o . ponto
+    var vel = Number(txtv.value);
+    res.innerHTML = 'Sua vlocidade atual é de ${vel}';
+}
+
 alert(`A velocidade do seu Carro é ${velocidade}Km/h`);
 if (velocidade < 15){
     alert("Velocidade Normal! Dirija Sempre com cautela.");
