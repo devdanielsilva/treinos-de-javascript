9ce0701a8969988f9d62b6bc24a23c643bb362f1
Daniel Silva <devdanielsilva@gmail.com>
2023-10-13 15:41:19 -0300

    NAO USE VAR, USE LET

--- ex013-condicoes-parte1/javascript-ex013.js
+++ ex013-condicoes-parte1/javascript-ex013.js
@@ -3,7 +3,9 @@ function calcular() {
     let txtv = document.querySelector('input#txtvel');
     let res = document.querySelector('div#res')//Para acessar o id no querySelector, se usa o #
     //Id usa # e Class se usa o . ponto
-    let vel = Number(txtv.value);
+
+    let vel = Number(txtv.value);//Usar let é melhor pois, se em um programa maior você acidentalmente declarar a mesma variável 2 ou mais vezes, você vai saber desse erro. O let te dá esses erros, diferente do var que não faz isso.
+
     res.innerHTML = `<br><p>Sua velocidade atual é de <strong>${vel} Km/h</strong></p>`;//O innerHTML pode ser usado para receber o conteúdo de um elemento HTML ou para definir um novo conteúdo para ele. Para isso, podemos utilizar a sintaxe do DOM (document. getElement) para realizar essas ações através do JavaScript.
 
     /*ESTRUTURA DE CONDIÇÃO SIMPLES, APENAS COM IF (SE). */
