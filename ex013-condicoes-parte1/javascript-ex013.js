4908f1b74d9a96f0b488acb02037d6d6d6177701
Daniel Silva <devdanielsilva@gmail.com>
2023-10-13 15:29:40 -0300

    Update javascript-ex013.js

--- ex013-condicoes-parte1/javascript-ex013.js
+++ ex013-condicoes-parte1/javascript-ex013.js
@@ -4,11 +4,11 @@ function calcular() {
     var res = document.querySelector('div#res')//Para acessar o id no querySelector, se usa o #
     //Id usa # e Class se usa o . ponto
     var vel = Number(txtv.value);
-    res.innerHTML = `<p>Sua velocidade atual é de <strong>${vel} Km/h</strong></p>`;//O innerHTML pode ser usado para receber o conteúdo de um elemento HTML ou para definir um novo conteúdo para ele. Para isso, podemos utilizar a sintaxe do DOM (document. getElement) para realizar essas ações através do JavaScript.
-    
+    res.innerHTML = `<br><p>Sua velocidade atual é de <strong>${vel} Km/h</strong></p>`;//O innerHTML pode ser usado para receber o conteúdo de um elemento HTML ou para definir um novo conteúdo para ele. Para isso, podemos utilizar a sintaxe do DOM (document. getElement) para realizar essas ações através do JavaScript.
+
     /*ESTRUTURA DE CONDIÇÃO SIMPLES, APENAS COM IF (SE). */
     if(vel > 60){
-        res.innerHTML += '<p>Você está <strong>MULTADO</strong> por excesso de velocidade.</p>';
+        res.innerHTML += '<br><p>Você está <strong>MULTADO</strong> por excesso de velocidade.</p>';
     } 
-    res.innerHTML += `<p>Dirija sempre com <strong>cinto</strong> de segurança!</p>`;
+    res.innerHTML += `<br><p>Dirija sempre com <strong>cinto</strong> de segurança!</p>`;
 }
