89e7881295a38fef04868efa31ad0d62af917daa
Daniel Silva <devdanielsilva@gmail.com>
2023-10-13 12:13:06 -0300

    .

--- ex014-condicao-composta/ex014.js
+++ ex014-condicao-composta/ex014.js
@@ -1,5 +1,5 @@
-var país = 'Brasil';
-document.write(`Vivendo em ${país}`);/*Aspas com crase, podemos concatenar com a variável usando o símbolo de cifrão $ e sinais de chaves, {} Abre chaves e fecha chaves. E fica assim: ${Nome da Variável aqui dentro}; */
+var país = prompt('De que país você é? ');
+document.write(`Vivendo em ${país}. `);/*Aspas com crase, podemos concatenar com a variável usando o símbolo de cifrão $ e sinais de chaves, {} Abre chaves e fecha chaves. E fica assim: ${Nome da Variável aqui dentro}; */
 /*Condição Composta - Com IF e ELSE. */
 if(país == 'Brasil'){//SE
     document.write('Você é Brasileiro(a)!');//Mensagem para Brasileiros.
