986c8aae9a34a257153a7f61bf05276ac55fc858
Daniel Silva <devdanielsilva@gmail.com>
2023-10-15 19:12:52 -0300

    Update ex014.js

--- ex014-condicao-composta/ex014.js
+++ ex014-condicao-composta/ex014.js
@@ -1,15 +1,15 @@
 function calcular() {
-  let país = document.querySelector("input#txtpaís");
-  let res = document.querySelector("div#res");
+  let país = document.querySelector('input#txtpaís');
+  let res = document.querySelector('div#res');
   /*Aspas com crase, podemos concatenar com a variável usando o símbolo de cifrão $ e sinais de chaves, {} Abre chaves e fecha chaves. E fica assim: ${Nome da Variável aqui dentro}; */
   /*Condição Composta - Com IF e ELSE. */
 
-  if (país != "Brasil") {
+  if (país != 'Brasil') {
     /*SE. != É sinal de diferente.
     Ao utilizar o sinal de diferente != , a mensagem que vai para o usuário no IF , vai inverte a mensagem de você é Brasileiro(a) para você é Estrangeiro e no ELSE ,também inverte, vai a mensagem de você é Brasileiro(a).*/
-    res.innerHTML = "Você é <strong>Estrangeiro(a)</strong>!"; //Mensagem para Estrangeiros.
+    res.innerHTML = 'Você é <strong>Estrangeiro(a)</strong>!'; //Mensagem para Estrangeiros.
   } else {
     //SENÃO
-    res.innerHTML = "Você é <strong>Brasileiro(a)</strong>!"; //Mensagem para brasileiro.
+    res.innerHTML = 'Você é <strong>Brasileiro(a)</strong>!'; //Mensagem para brasileiro.
   }
 }
