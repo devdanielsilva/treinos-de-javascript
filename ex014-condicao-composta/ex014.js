52f16cab17b61f6d68486756bb2f130aae5c1dcb
Daniel Silva <devdanielsilva@gmail.com>
2023-10-18 20:36:11 -0300

    Update ex014.js

--- ex014-condicao-composta/ex014.js
+++ ex014-condicao-composta/ex014.js
@@ -1,5 +1,6 @@
 function calcular() {
-  let país = document.querySelector("input#txtpaís").value;
+  let país = document.querySelector("input#txtpaís").value;/*O atributo value para definir o valor enviado por este item. Use o atributo checked para indicar se o item está selecionado por padrão. O value é a informação que será enviado pro servidor.*/
+
   let res = document.querySelector("div#res");
   /*Aspas com crase, podemos concatenar com a variável usando o símbolo de cifrão $ e sinais de chaves, {} Abre chaves e fecha chaves. E fica assim: ${Nome da Variável aqui dentro}; */
   /*Condição Composta - Com IF e ELSE. */
@@ -7,6 +8,7 @@ function calcular() {
   if (país != "Brasil") {
     /*SE. != É sinal de diferente.
     Ao utilizar o sinal de diferente != , a mensagem que vai para o usuário no IF , vai inverte a mensagem de você é Brasileiro(a) para você é Estrangeiro e no ELSE ,também inverte, vai a mensagem de você é Brasileiro(a).*/
+    
     res.innerHTML = "<p>Você é <strong>Estrangeiro(a)</strong>!</p>"; //Mensagem para Estrangeiros.
   } else {
     //SENÃO
