9c25c46c42c827d8fea0e6784755db06a2f3de5f
Daniel Silva <devdanielsilva@gmail.com>
2023-12-29 23:01:29 -0300

    Update ex015.js

--- ex015-condicoes-parte2/js/ex015.js
+++ ex015-condicoes-parte2/js/ex015.js
@@ -2,7 +2,8 @@ var idade = 68;
 console.log(`Você tem ${idade} anos.`)
 if (idade < 16) {
   console.log(`Não vota!`);
-} else if (idade < 18 || idade > 65){ /*Usando else if, para economizar um bloco de código, economizar escrita. E porque if e else é muito usado.*/ 
+} else if (idade < 18 || idade > 65){ /*Usando else if, para economizar um bloco de código, economizar escrita. E porque if e else é muito usado.
+No Brasil quem tem meno de 18 anos vota e mais de 65 anos.*/ 
   console.log(`Voto opcional!`);
 } else { /*Se a idade é > 18 maior que 18 anos, é obrigatório o voto.
 Então não precisa do if (idade >= 18) é uma questão de lógica.*/
