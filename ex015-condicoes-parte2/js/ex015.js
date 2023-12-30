c9f40e881fde9f6f047405058720063140aebe01
Daniel Silva <devdanielsilva@gmail.com>
2023-12-29 22:37:08 -0300

    Update ex015.js

--- ex015-condicoes-parte2/js/ex015.js
+++ ex015-condicoes-parte2/js/ex015.js
@@ -1,8 +1,9 @@
-var idade = 18;
+var idade = 68;
+console.log(`Você tem ${idade} anos.`)
 if (idade < 16) {
   console.log(`Não vota!`);
-} else if (idade < 18) { /*Usando else if, para economizar um bloco de código, economizar escrita. E porque if e else é muito usado.*/ 
+} else if (idade < 18 || idade > 65){ /*Usando else if, para economizar um bloco de código, economizar escrita. E porque if e else é muito usado.*/ 
   console.log(`Voto opcional!`);
-} else if (idade > 18) {
+} else {
   console.log(`Voto Obrigatório!`);
 }
