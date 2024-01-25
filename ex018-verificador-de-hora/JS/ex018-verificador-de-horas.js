7760abec13fb3692c31e4e193dfadf666dd95235
Daniel Silva <devdanielsilva@gmail.com>
2024-01-25 18:52:09 -0300

    ajustes / anotações

--- ex018-verificador-de-hora/JS/ex018-verificador-de-horas.js
+++ ex018-verificador-de-hora/JS/ex018-verificador-de-horas.js
@@ -1,13 +1,15 @@
 function carregar() {
   var msg = document.getElementById("msg");
   var img = document.getElementById("imagem");
-  var data = new Date();
-  var hora = data.getHours();
+  var data = new Date(); //Data atual
+  var hora = data.getHours(); //Hora atual
   msg.innerHTML = `Agora são ${hora} horas.`;
 
   if (hora >= 0 && hora < 12) {
     //Bom Dia!
     document.body.style.background = "#e2cd9f";
+    /* document.body.style.background = "#e2cd9f"; serve para podermos mudar a cor de fundo da página inteira em determinado momento ou condição. */
+
     msg.innerHTML += " Tenha um Bom Dia!";
     img.src = "imagens/Manhã.jpg";
   } else if (hora >= 12 && hora <= 17) {
