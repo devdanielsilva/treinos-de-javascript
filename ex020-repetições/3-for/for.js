651c832c3f011bdd4a61075762627f3d87c12812
Daniel Silva <devdanielsilva@gmail.com>
2024-01-31 23:33:26 -0300

    Estrutura for
    Estrutura de repetição de variável de controle

---
+++ ex020-repetições/3-for/for.js
@@ -0,0 +1,4 @@
+/*Colocar só o sinal de menor < o resultado vai ficar de 1 até 4, mas se colocar o sinal de menor e igual <= o resultado vai ser de 1 até 5. */
+for (var c = 1; c <= 5; c++) {
+  console.log(`Passo ${c}`);
+}
